9c033fc5f7825863bc821dc899ad790b73416826
flasticky <jan.flaska@gmail.com>
2024-03-03 15:25:52 +0100

    feat: Create loop for generating passwords

--- main.py
+++ main.py
@@ -38,33 +38,34 @@ def generatePassword(minLength, numbers=True, specialCharacters=True):
             meetsCriteria = meetsCriteria and containsSpecial
     
     return secret
- 
-while True:   
-    minLength = int(input("Enter the length (min. length 5, max. length 100): "))
-    if minLength >= 5 and minLength <= 100:
-        break
-    else:
-        if minLength < 5:
-            print("Your password is too short.")
+
+while True:
+    while True:   
+        minLength = int(input("Enter the length (min. length 5, max. length 100): "))
+        if minLength >= 5 and minLength <= 100:
+            break
         else:
-            print("Your password is too long.")
+            if minLength < 5:
+                print("Your password is too short.")
+            else:
+                print("Your password is too long.")
 
-if minLength < 10:
-    print("WARNING - Your password is too short. Please consider a longer password for better security.\n")
+    if minLength < 10:
+        print("WARNING - Your password is too short. Please consider a longer password for better security.\n")
 
-while True:
-    containsNumber = input("Do you want to include numbers (y/n)? ").lower()
-    if containsNumber == "y" or containsNumber == "n":
-        break
-    else:
-        print("Please enter 'y' for yes or 'n' for no.")
+    while True:
+        containsNumber = input("Do you want to include numbers (y/n)? ").lower()
+        if containsNumber == "y" or containsNumber == "n":
+            break
+        else:
+            print("Please enter 'y' for yes or 'n' for no.")
 
-while True:
-    containsSpecial = input("Do you want to include special characters (y/n)? ").lower()
-    if containsSpecial == "y" or containsSpecial == "n":
-        break
-    else:
-        print("Please enter 'y' for yes or 'n' for no.")
+    while True:
+        containsSpecial = input("Do you want to include special characters (y/n)? ").lower()
+        if containsSpecial == "y" or containsSpecial == "n":
+            break
+        else:
+            print("Please enter 'y' for yes or 'n' for no.")
 
-secret = generatePassword(minLength, containsNumber == "y", containsSpecial == "y")
-print("The generated password is:", secret)
+    secret = generatePassword(minLength, containsNumber == "y", containsSpecial == "y")
+    print("The generated password is:", secret, "\n")
