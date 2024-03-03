8759ae24082ce532a790565ca28157e0b8db34ae
flasticky <jan.flaska@gmail.com>
2024-03-03 15:09:59 +0100

    feat: User input checks

--- main.py
+++ main.py
@@ -33,11 +33,30 @@ def generatePassword(minLength, numbers=True, specialCharacters=True):
             meetsCriteria = meetsCriteria and containsSpecial
     
     return secret
-    
-minLength = int(input("Enter the length: "))
+ 
+while True:   
+    minLength = int(input("Enter the length (min. length 5, max. length 100): "))
+    if minLength >= 5 and minLength <= 100:
+        break
+    else:
+        print("Your password is too long or too short.")
+
 if minLength < 10:
-    print("WARNING - Your password is too short. Please choose a longer password for better security.")
-containsNumber = input("Do you want to include numbers (y/n)? ").lower() == "y"
-containsSpecial = input("Do you want to include special characters (y/n)? ").lower() == "y"
-secret = generatePassword(minLength, containsNumber, containsSpecial)
+    print("WARNING - Your password is too short. Please consider a longer password for better security.\n")
+
+while True:
+    containsNumber = input("Do you want to include numbers (y/n)? ").lower()
+    if containsNumber == "y" or containsNumber == "n":
+        break
+    else:
+        print("Please enter 'y' for yes or 'n' for no.")
+
+while True:
+    containsSpecial = input("Do you want to include special characters (y/n)? ").lower()
+    if containsSpecial == "y" or containsSpecial == "n":
+        break
+    else:
+        print("Please enter 'y' for yes or 'n' for no.")
+
+secret = generatePassword(minLength, containsNumber == "y", containsSpecial == "y")
 print("The generated password is:", secret)
